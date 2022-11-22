64fe4a3ccfa151812138b06f55e8232fbe04d411
rlgustavo <rlgustavo1927@gmail.com>
2022-11-22 16:42:06 -0300

    a

--- php/enviar.php
+++ php/enviar.php
@@ -1,12 +1,11 @@
 <?php
-  //Variáveis
   $nome = $_POST['nome'];
   $email = $_POST['email'];
   $mensagem = $_POST['mensagem'];
   $data_envio = date('d/m/Y');
   $hora_envio = date('H:i:s');
 
-  //Compo E-mail
+
   $arquivo = "
     <html>
       <p><b>Nome: </b>$nome</p>
@@ -16,16 +15,15 @@
     </html>
   ";
   
-  //Emails para quem será enviado o formulário
   $destino = "shadowfoxfatec@gmail.com";
   $assunto = "Contato pelo Site";
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+ 
   $headers  = "MIME-Version: 1.0\n";
   $headers .= "Content-type: text/html; charset=iso-8859-1\n";
   $headers .= "From: $nome <$email>";
 
-  //Enviar
+
   mail($destino, $assunto, $arquivo, $headers);
   
   echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
